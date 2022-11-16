47153f20acc21a5cea15833a07b88f80d3853a4d
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2022-11-16 03:19:17 +0200

    added padding to timer.tsx

--- public/components/InputTime.tsx
+++ public/components/InputTime.tsx
@@ -1,47 +1,41 @@
-import React from "react";
-// import { timerProps } from "./TimerInterface";
 import { useEffect, useState, useRef } from "react";
 import Timer from "./Timer";
 
 const InputTimer = () => {
   const [timerFinished, setTimerFinished] = useState(true);
-  const [isMicroBreak, setIsMicroBreak] = useState(false);
   const [isTimeSet, setIsTimeSet] = useState(false);
   const [prevTime, setPrevTime] = useState(null);
   const [seconds, setSeconds] = useState(1500);
   const interval = useRef<any>(null);
   const [hours, setHours] = useState(20);
   const [minutes, setMinutes] = useState(20);
+  const isMicroBreak = useRef(false);
+  const tenSecBreak = useRef(0);
+  const totalTimeCounter = useRef(0);
+  const timeToMicroBreak = useRef(0);
 
   const submitHandler = (e: any) => {
     e.preventDefault();
-    microBreakCounter = 0;
-    totalTimeCounter = 0;
+    totalTimeCounter.current = 0;
+    timeToMicroBreak.current = 1;
+    tenSecBreak.current = 0;
     setIsTimeSet(true);
-    setSeconds(
-      e.target[2].value + e.target[1].value * 60 + e.target[0].value * 3600
-    );
-    console.log(e.target[0].value * 3600);
-    console.log(e.target[1].value * 60);
-    console.log(e.target[2].value);
+    setSeconds(e.target[2].value);
+    setMinutes(e.target[1].value);
+    setHours(e.target[0].value);
   };
-
-  let microBreakCounter = 0;
-  let totalTimeCounter = 0;
-  let timeToMicroBreak = 0;
-  let microBreak = false;
 
   const timeUntilMicroBreak = () => {
     let timeRandomizationDeviation = Math.floor(Math.random() * 20);
-    timeToMicroBreak = 30;
+    timeToMicroBreak.current = 30;
     const plusOrMinus = Math.random() < 0.5;
     if (plusOrMinus) {
-      timeToMicroBreak += timeRandomizationDeviation;
-      console.log("nextMicroBreak = " + timeToMicroBreak);
+      timeToMicroBreak.current += timeRandomizationDeviation;
+      // console.log("nextMicroBreak = " + timeToMicroBreak.current);
       return timeToMicroBreak;
     } else {
-      timeToMicroBreak -= timeRandomizationDeviation;
-      console.log("nextMicroBreak = " + timeToMicroBreak);
+      timeToMicroBreak.current -= timeRandomizationDeviation;
+      // console.log("nextMicroBreak = " + timeToMicroBreak.current);
       return timeToMicroBreak;
     }
   };
@@ -52,13 +46,41 @@ const InputTimer = () => {
       audio.play();
     }
   };
+  useEffect(() => {
+    breakAlert();
+  }, [isMicroBreak]);
 
+  if (seconds <= 0 && minutes > 0) {
+    setSeconds((prevSeconds) => prevSeconds + 59);
+    setMinutes((prevMinutes) => prevMinutes - 1);
+  }
+  if (minutes <= 0 && hours > 0) {
+    setMinutes((prevMinutes) => prevMinutes + 59);
+    setHours((prevHours) => prevHours - 1);
+  }
+  if (totalTimeCounter.current === timeToMicroBreak.current) {
+    isMicroBreak.current = true;
+    totalTimeCounter.current = 0;
+    timeUntilMicroBreak();
+  }
   useEffect(() => {
     if (isTimeSet) {
-      interval.current = setInterval(
-        () => setSeconds((prevSeconds) => prevSeconds - 1),
-        1000
-      );
+      timeUntilMicroBreak();
+      interval.current = setInterval(() => {
+        setSeconds((prevSeconds) => prevSeconds - 1);
+        totalTimeCounter.current = totalTimeCounter.current + 1;
+        // timeToMicroBreak.current = timeToMicroBreak.current + 1;
+        if (isMicroBreak) {
+          tenSecBreak.current = tenSecBreak.current + 1;
+          // console.log("tensecBreak" + tenSecBreak.current);
+          if (tenSecBreak.current >= 10) {
+            isMicroBreak.current = false;
+            tenSecBreak.current = 0;
+          }
+        }
+        // console.log("totTime " + totalTimeCounter.current);
+        // console.log("microbreak at " + timeToMicroBreak.current);
+      }, 1000);
     }
     return () => clearInterval(interval.current);
   }, [isTimeSet]);
@@ -68,8 +90,8 @@ const InputTimer = () => {
   };
 
   useEffect(() => {
-    if (seconds <= 0) {
-      setIsTimeSet(false);
+    if (seconds <= 0 && minutes <= 0 && hours <= 0) {
+      setTimerFinished(true);
       clearInterval(interval.current);
     }
   }, [seconds]);
@@ -124,9 +146,7 @@ const InputTimer = () => {
                 <button type="submit" value="Submit" className="time-input">
                   Start
                 </button>
-                <button onClick={breakAlert} className="time-input">
-                  sound
-                </button>
+                <button className="time-input">sound</button>
               </form>
             </div>
           </div>
@@ -136,8 +156,7 @@ const InputTimer = () => {
           <button onClick={cancelTimerHandler} className="time-input">
             cancel
           </button>
-          {seconds}
-          {microBreak && <p>{microBreakCounter}</p>}
+          {isMicroBreak && <p>BREAK</p>}
           <Timer hours={hours} minutes={minutes} seconds={seconds} />
         </>
       )}
--- public/components/Timer.tsx
+++ public/components/Timer.tsx
@@ -6,39 +6,58 @@ export interface timerProps {
   seconds: number;
 }
 
-const Timer = ({ seconds }: timerProps) => {
-  const [time, setTime] = useState({
-    // hours: 0,
-    minutes: 0,
-    seconds: "0",
+const Timer = ({ seconds, minutes, hours }: timerProps) => {
+  const [time, setTime] = useState<any>({
+    hours: "00",
+    minutes: "00",
+    seconds: "00",
   });
 
   useEffect(() => {
     setTime({
-      // hours: Math.floor(seconds / 3600),
-      minutes: Math.floor(seconds / 60),
-      seconds: convertSeconds(),
+      seconds: paddedNumbers(seconds),
+      minutes: paddedNumbers(minutes),
+      hours: paddedNumbers(hours),
     });
-    // console.log(time.hours);
     console.log(time.minutes);
-    console.log(time.seconds);
+    console.log(minutes);
   }, [seconds]);
 
-  const convertSeconds = () => {
-    const convertedSeconds = Math.round(
-      (seconds / 60 - Math.floor(seconds / 60)) * 60
-    );
-    let paddedSeconds = convertedSeconds.toString();
-    if (paddedSeconds.length < 2) paddedSeconds = "0".concat("", paddedSeconds);
-
-    return paddedSeconds;
+  const paddedNumbers = (number: number) => {
+    let paddedNumbers = number.toString();
+    if (paddedNumbers.length < 2) {
+      paddedNumbers = "0".concat("", paddedNumbers);
+      console.log(paddedNumbers);
+    }
+    return paddedNumbers;
   };
+
+  // const convertSeconds = () => {
+
+  //   const convertedSeconds = Math.round(
+  //     (seconds / 60 - Math.floor(seconds / 60)) * 60
+  //   );
+  //   let paddedSeconds = convertedSeconds.toString();
+  //   if (paddedSeconds.length < 2) paddedSeconds = "0".concat("", paddedSeconds);
+
+  //   return paddedSeconds;
+  // };
+
+  // const convertMinutes = () => {
+  //   const convertedMinutes = Math.round(
+  //     (seconds / 60 - Math.floor(seconds / 60)) * 60
+  //   );
+  //   let paddedSeconds = convertedMinutes.toString();
+  //   if (paddedSeconds.length < 2) paddedSeconds = "0".concat("", paddedSeconds);
+
+  //   return paddedSeconds;
+  // };
 
   return (
     <div className="timer-wrapper">
       <div className="timer-inner">
         <div className="timer-segment">
-          {/* <span className="time">{time.hours}</span> */}
+          <span className="time">{time.hours}</span>
           <span className="label">:</span>
         </div>
         <div className="timer-segment">
